d4b9c1100b3e5123bd312f2284c43b6f368849fa
drystar <chris.drysdale12@gmail.com>
2019-10-16 20:23:20 +0000

    head

---
+++ head.js
@@ -0,0 +1,16 @@
+// insert assertEqual function
+
+var firstElement = array1.shift();
+
+console.log(array1);
+// expected output: Array [2, 3]
+
+console.log(firstElement);
+// expected output: 1
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse
+
+
+_.first([1, 2, 3]);
+// => [1]
