1b3612dd9a9c77d33ba8133d68e1ee36baab5b66
drystar <chris.drysdale12@gmail.com>
2019-10-18 03:04:53 +0000

    map.js

--- map.js
+++ map.js
@@ -1,25 +1,3 @@
-// Insert assertEqual Function
-const assertEqual = function(actual, expected) {
-  // print this one to the console if 'actual' is same as expected
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  // print this one to the console if 'actual' is not same as expected.=
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Implement a function eqArrays
-
-function eqArrays(_arr1, _arr2) {
-
-  if (!Array.isArray(_arr1) || ! Array.isArray(_arr2) || _arr1.length !== _arr2.length)
-    return false;
-
-    console.log("im confused", eqArrays)
-
-};
-
 const words = ["ground", "control", "to", "major", "tom"];
 
 // Start off with an empty array and return that array
@@ -48,6 +26,6 @@ const map = function(array, callback) {
   }
   return results;
 }
-assertEqual(console.log(map(words, printFirst)))
+console.log(map(words, printFirst))
 
 
