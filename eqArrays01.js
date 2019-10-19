a1f8b1dfbc618103400f7b038aa3b107392a43ce
drystar <chris.drysdale12@gmail.com>
2019-10-19 23:29:26 +0000

    eqArrays01

--- eqArrays01.js
+++ eqArrays01.js
@@ -2,27 +2,34 @@
 const assertEqual = function(actual, expected) {
   // print this one to the console if 'actual' is same as expected
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   // print this one to the console if 'actual' is not same as expected.=
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // Implement a function eqArrays which takes in two arrays and 
 // returns true or false, based on a perfect match
 
+const eqArrays = function(arrayA, arrayB) {
+  
+  // var a = ([1, 2, 3], [1, 2, 3]); 
+  //var b = ([1, 2, 3], [3, 2, 1]);
+   
+  // if the length is not equal 
+   if(arrayA.length !== arrayB.length) {
+    return false; 
+   }
+   // comparing each element of the array 
+    for(var i = 0; i < arrayA.length; i++) {
+    if(arrayA[i] !== arrayB[i]) {
+     return false;
+   }
+  }
+  return true; 
+};
 
-const eqArrays = function(a, b) {
-  
- // var a = ([1, 2, 3], [1, 2, 3]); 
- //var b = ([1, 2, 3], [3, 2, 1]);
-  
- // if the length is not equal 
-  if(a.length!=b.length) 
-   return false; 
-  else
-  { 
   // comparing each element of the array 
    for(var i=0;i<a.length;i++) 
    if(a[i]!=b[i]) 
