2f5163c0b4594364e2a6f3f1397498b21f75cd4d
drystar <chris.drysdale12@gmail.com>
2019-10-19 23:22:12 +0000

    assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,26 @@
 // FUNCTION IMPLEMENTATION
+// Insert assertEqual Function
+
 const assertEqual = function(actual, expected) {
   // print this one to the console if 'actual' is same as expected
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   // print this one to the console if 'actual' is not same as expected.=
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
+// const assertArraysEqual = function(arrayA, arrayB) {
+//   // print this one to the console if 'actual' is same as expected
+//   if (eqArrays(arrayA, arrayB)) {
+//     console.log(`✅✅✅Assertion Passed: ${arrayA} === ${arrayB}`);
+//   // print this one to the console if 'actual' is not same as expected.=
+//   } else {
+//     console.log(`🛑🛑🛑Assertion Failed: ${arrayA} !== ${arrayB}`);
+//   }
+// };
 
 // TEST CODE
 
