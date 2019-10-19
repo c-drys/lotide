137f6f2ed08c89d18d9798ce562acdad8ababdcd
drystar <chris.drysdale12@gmail.com>
2019-10-19 23:14:56 +0000

    eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,7 @@
 const assertEqual = function(actual, expected) {
   // print this one to the console if 'actual' is same as expected
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(:white_tick::white_tick::white_tick:`Assertion Passed: ${actual} === ${expected}`);
   // print this one to the console if 'actual' is not same as expected.=
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
