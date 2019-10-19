9bf95db111fda750c48a778aa4c178a8da48c319
drystar <chris.drysdale12@gmail.com>
2019-10-19 23:14:35 +0000

    assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,37 +1,50 @@
+// Implement a function eqArrays
+
+const eqArrays = function(arrayA, arrayB) {
+  
+  // var a = ([1, 2, 3], [1, 2, 3]); 
+  //var b = ([1, 2, 3], [3, 2, 1]);
+   
+  // if the length is not equal 
+   if(arrayA.length !== arrayB.length) {
+    return false; 
+   }
+   // comparing each element of the array 
+    for(var i = 0; i < arrayA.length; i++) {
+    if(arrayA[i] !== arrayB[i]) {
+     return false;
+   }
+  }
+  return true; 
+};
 // Insert assertEqual Function
-const assertEqual = function(actual, expected) {
+const assertArraysEqual = function(arrayA, arrayB) {
   // print this one to the console if 'actual' is same as expected
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`✅✅✅Assertion Passed: ${arrayA} === ${arrayB}`);
   // print this one to the console if 'actual' is not same as expected.=
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Assertion Failed: ${arrayA} !== ${arrayB}`);
   }
 };
 
-// Implement a function eqArrays
-
-function eqArrays(_arr1, _arr2) {
-
-  if (!Array.isArray(_arr1) || ! Array.isArray(_arr2) || _arr1.length !== _arr2.length)
-    return false;
-
-    console.log("im confused", eqArrays)
-
-};
 
 
 // Implement a function for equal array comparison
 
-function AseqArrays(_arr1, _arr2) {
-  if (_arr1.length !== _arr2.length) 
-  return false;
+// function AseqArrays(_arr1, _arr2) {
+//   if (_arr1.length !== _arr2.length) 
+//   return false;
 
-  for (var i = 0, e = _arr1.length; i < e; i++) {
-    if (_arr1[i] !== _arr2[i] {
-    return false;
-    }
-  }
-  return true;
-}
+//   for (var i = 0, e = _arr1.length; i < e; i++) {
+//     if (_arr1[i] !== _arr2[i]) {
+//       return false;
+//     }
 
+//   }
+//   return true;
+// }
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
