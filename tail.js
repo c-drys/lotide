7a6c8e6a61d95af00826435e1b3ab712eef7829d
drystar <chris.drysdale12@gmail.com>
2019-10-21 20:04:52 +0000

    tail

--- tail.js
+++ tail.js
@@ -1,26 +1,13 @@
-// Insert assertEqual Function
-const assertEqual = function(actual, expected) {
-  // print this one to the console if 'actual' is same as expected
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  // print this one to the console if 'actual' is not same as expected.=
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 // Implement function to return the tail 
 // Declare the empty array
 // Array.slice will remove 1st element & return new tail array
 
   const words = ["Yo Yo", "Lighthouse", "Labs"];
-  tail = words.slice(1);
-    console.log(tail)
+  const tail = function (array) {
+  return array.slice(1);
 
+  };
 
-  // console.log(assertEqual(tail([5,6,7]), [6, 7]));
-  // console.log(assertEqual(tail(["Lighthouse", "Labs"]))); // => will always fail!
-
-
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
