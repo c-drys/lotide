a07ce564c6b6c87d889f8d8a56e7fd016c9a0896
drystar <chris.drysdale12@gmail.com>
2019-10-21 23:27:41 +0000

    index

---
+++ index.js
@@ -0,0 +1,38 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const map = require('./map');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  map: map,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without
+
+
+};
