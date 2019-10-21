05a39aa0b7908cca6f3bb70ee519c73cfcd5dc22
drystar <chris.drysdale12@gmail.com>
2019-10-21 05:59:05 +0000

    assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,33 +1,53 @@
-// eqObjects
+///Insert eqArrays Function
 
-/const eqObjects = function(object1, object2) {
-  if (object1.keys(object1).length !== object1.keys(object2).length){
+const eqArrays = function(arrayA, arrayB) {
+  
+  if(arrayA.length !== arrayB.length) {
+   return false; 
+  }
+   for(var i = 0; i < arrayA.length; i++) {
+   if(arrayA[i] !== arrayB[i]) {
     return false;
   }
+ }
+ return true; 
+};
 
-  for (key of object1.keys(object1)){
-    if (array.isArray(object1[key]) && array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
-    
-    } else { 
-      if (object1[key] !== object2[key] {
-      }
-        return false;
-      }
-    }
-  }
-  return true;
-  }
+// Insert eqObjects
+
+const eqObjects = function(object1, object2) {
+ if (Object.keys(object1).length !== Object.keys(object2).length) {
+   return false;
+ }
+
+ for(const key in object1) {
+   if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
+     console.log(object1[key]);
+     console.log(object2[key]);
+     if (eqArrays(object1[key], object2[key]) === false) { //eqArrays called here.
+       return false;
+     }
+   } else if (object1[key] !== object2[key]) {
+     return false;
+   }
+ }
+return true;
+};
   
-  / Function implementation
+// Function implementation
 
 const asertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
+  const inspect = require('util').inspect; 
+  // if console.log (inspect then object, it will call )
 
-  if (actual === true) {
-    console.log('✅✅✅Assertion Passed; ${actual} === ${expected}');
+  if (eqObjects(actual, expected)) {
+    console.log('✅✅✅Assertion Passed; ${inspect(actual)} === ${inspect(expected)}');
+    // print this one to the console if 'actual' is same as expected
   } else {
-    console.log('🛑🛑🛑Assertion Failed: ${actual} !== ${expected}');
+    console.log('🛑🛑🛑Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}');
+    // print this one to the console if 'actual' is not same as expected.=
   } 
 };
+
+// inspects accepts an object and coverts to a readable String
 
