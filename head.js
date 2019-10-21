16f1769280c5cc547ad21fb79e0c1f356b005d7c
drystar <chris.drysdale12@gmail.com>
2019-10-21 19:51:44 +0000

    head

--- head.js
+++ head.js
@@ -1,24 +1,17 @@
-// Insert assertEqual Function
-const assertEqual = function(actual, expected) {
-  // print this one to the console if 'actual' is same as expected
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  // print this one to the console if 'actual' is not same as expected.=
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 // Implement function to return the head 
 // Declare the empty array
 // Array.shift will remove & return 1st element 
 
-const arr = ([])
-let head = arr.shift()
-  console.log(head); //undefined as predicted
+// const arr = ([])
+// let head = arr.shift()
+//   console.log(head); //undefined as predicted
 
+const head = function (array) {
+  return array[0]; // index 0 - head 
+};
 
-// // Test assertions
-assertEqual(arr.shift([5,6,7]), 5);
-assertEqual(arr.shift(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(arr.shift(["Mon", "Tue", "Wed", "Thurs"]), "Mon")
+// array.shift not as clean
+// head is a function 
+
+module.exports = head;
