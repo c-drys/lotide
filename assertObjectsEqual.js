53cc0328a9a89bcac5ad83ba1d702ca867223f90
drystar <chris.drysdale12@gmail.com>
2019-10-22 01:17:01 +0000

    assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,42 +1,9 @@
-///Insert eqArrays Function
-
-const eqArrays = function(arrayA, arrayB) {
-  
-  if(arrayA.length !== arrayB.length) {
-   return false; 
-  }
-   for(var i = 0; i < arrayA.length; i++) {
-   if(arrayA[i] !== arrayB[i]) {
-    return false;
-  }
- }
- return true; 
-};
-
-// Insert eqObjects
-
-const eqObjects = function(object1, object2) {
- if (Object.keys(object1).length !== Object.keys(object2).length) {
-   return false;
- }
-
- for(const key in object1) {
-   if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
-     console.log(object1[key]);
-     console.log(object2[key]);
-     if (eqArrays(object1[key], object2[key]) === false) { //eqArrays called here.
-       return false;
-     }
-   } else if (object1[key] !== object2[key]) {
-     return false;
-   }
- }
-return true;
-};
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
   
 // Function implementation
 
-const asertObjectsEqual = function(actual, expected) {
+const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; 
   // if console.log (inspect then object, it will call )
 
@@ -51,3 +18,4 @@ const asertObjectsEqual = function(actual, expected) {
 
 // inspects accepts an object and coverts to a readable String
 
+module.exports = assertObjectsEqual;
