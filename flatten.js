cf56f268c4dadddf5eb1094dac7b2051dec1eeca
drystar <chris.drysdale12@gmail.com>
2019-10-22 01:22:06 +0000

    flatten

--- flatten.js
+++ flatten.js
@@ -1,32 +1,6 @@
-// Insert eqAssays & assertEqual Function
-
-const eqArrays = function(arrayA, arrayB) {
-  
-  // var a = ([1, 2, 3], [1, 2, 3]); 
-  //var b = ([1, 2, 3], [3, 2, 1]);
-   
-  // if the length is not equal 
-   if(arrayA.length !== arrayB.length) {
-    return false; 
-   }
-   // comparing each element of the array 
-    for(var i = 0; i < arrayA.length; i++) {
-    if(arrayA[i] !== arrayB[i]) {
-     return false;
-   }
-  }
-  return true; 
-};
-
-const assertArraysEqual = function(arrayA, arrayB) {
-  // print this one to the console if 'actual' is same as expected
-  if (eqArrays(arrayA, arrayB)) {
-    console.log(`✅✅✅Assertion Passed: ${arrayA} === ${arrayB}`);
-  // print this one to the console if 'actual' is not same as expected.=
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${arrayA} !== ${arrayB}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 // is this an array?
 // if yes, return flattened x
@@ -57,3 +31,5 @@ assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 22, 3, 4, 55, 6]);
 
 
 // newArray [2][0] -- retrieves 3 ==> [1, 2, [3, 4], 5, [6]]
+
+module.exports = flatten;
