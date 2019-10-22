b2527ae7013b1a233bc65a223a782506167dbd7f
drystar <chris.drysdale12@gmail.com>
2019-10-22 01:22:56 +0000

    letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,38 +1,5 @@
-// Insert assertEqual Function
-const assertEqual = function (actual, expected) {
-  // print this one to the console if 'actual' is same as expected
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-    // print this one to the console if 'actual' is not same as expected.=
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Implement a function eqArrays which takes in two arrays and 
-// returns true or false, based on a perfect match
-
-
-const eqArrays = function (a, b) {
-
-  // var a = ([1, 2, 3], [1, 2, 3]); 
-  //var b = ([1, 2, 3], [3, 2, 1]);
-
-  // if the length is not equal 
-  if (a.length !== b.length) {
-    return false;
-  } else {
-    // comparing each element of the array 
-    for (var i = 0; i < a.length; i++) {
-      if (a[i] != b[i]) {
-        return false;
-      }
-    }
-      
-    return true;
-  }
-}
-
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const letterPositions = function (string) {
   let freq = {}
@@ -61,3 +28,5 @@ assertEqual(eqArrays(letterPositions("hello").e, [1]));
 
 
 // Define Object Structure To Return
+
+module.exports = letterPositions;
